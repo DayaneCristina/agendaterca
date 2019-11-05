5f008c7a4e2cd7e474aec5550e119d0ceea5eb82
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-05 12:32:08 -0300

    Update index.js

--- Frontend/src/pages/main/index.js
+++ Frontend/src/pages/main/index.js
@@ -6,6 +6,9 @@ import
   Row,
   Col,
   Card,
+  Nav,
+  Navbar,
+  NavDropdown
 } from 'react-bootstrap';
 
 const data = '';
@@ -22,17 +25,34 @@ export default class Main extends Component {
 
   render() {
     return (
-      <Container>
-        <Row>
-          <Col>
-            <Card className="shadow p-5 mb-5 bg-white rounded formLogin box">
-              <h1>Bem-vindo {this.data[0].Name.toString()}</h1>
-              <h3>Estamos carregando seus contatos, por favor aguarde um instante</h3>
-              
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Agenda Terça</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">Inicio</Nav.Link>
+            <NavDropdown title="Contatos" src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Adicio</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <NavDropdown title={this.data[0].Name.toString()} id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Adicio</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link id="collasible-nav-dropdown">
+              <img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png" width="50" height="50" className="rounded-circle"></img>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
