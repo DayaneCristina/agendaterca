123da41f0a7fd4a17bd71a7ee3eedd8e75780980
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-04 21:31:53 -0300

    Update agendaterca.sql

--- Database/agendaterca.sql
+++ Database/agendaterca.sql
@@ -1,3 +1,4 @@
+-- SQL Server
 create database agendaterca; -- cria o banco
 
 use agendaterca; --usar o banco
@@ -27,3 +28,64 @@ insert into phones(phone, person_id) values ('1332326677',1);
 insert into email(email, person_id) values ('mariajoaquina@carrossel.com.br', 1);
 
 select * from people, phones, email where people.id=phones.person_id and people.id = email.person_id;
+
+-- MYSQL 
+create database agendaterca; -- cria o banco
+
+use agendaterca; -- usar o banco
+
+create table people(
+	id_people int auto_increment, 
+    name varchar(255),
+    nickname VARCHAR(32),
+	birthday DATE,
+    constraint pk_id
+		primary key (id_people)
+);
+
+create table contact(
+	id_contact int auto_increment,
+    contact varchar(255),
+    tipy int,
+    constraint pk_id_contato 
+		primary key (id_contact)
+);
+
+create table tipy(
+	id_tipy int,
+    name varchar(255),
+    constraint pk_id_tipy
+		primary key (id_tipy)
+);
+
+create table user(
+	id_user int auto_increment,
+    name varchar(255),
+    email varchar(255),
+    password varchar(255),
+    apikey text,
+    constraint pk_id_user
+		primary key (id_user)
+);
+
+create table contact_people(
+	id_contact int,
+    id_people int,
+    constraint fk_contact_people
+		foreign key (id_contact)
+			references contact(id_contact),
+	constraint fk_people_contact
+		foreign key (id_people)
+			references people(id_people)
+);
+
+create table people_user(
+	id_people int,
+    id_user int,
+    constraint fk_user_people
+		foreign key (id_user)
+			references user(id_user),
+	constraint fk_people_user
+		foreign key (id_people)
+			references people(id_people)
+);
